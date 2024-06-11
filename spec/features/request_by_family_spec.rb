b512c6a4e5cf258995e2fa51538f2808f9da9f81
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-06-11 11:06:53 +0100

    Complete main paths

---
+++ spec/features/request_by_family_spec.rb
@@ -0,0 +1,88 @@
+require "rails_helper"
+
+RSpec.feature "Request by solicitor", type: :feature do
+  scenario "User makes end to end request" do
+    visit "/"
+
+    # Start page
+    click_link "Start now"
+
+    # Subject
+    choose "Someone else's"
+    click_button "Continue"
+
+    # Your details
+    fill_in("Full name", with: "John")
+    click_button "Continue"
+
+    # Date of birth
+    fill_in("Day", with: "1")
+    fill_in("Month", with: "1")
+    fill_in("Year", with: "2000")
+    click_button "Continue"
+
+    # Relationship
+    choose "Relative, friend or something else"
+    click_button "Continue"
+
+    # Your details
+    fill_in("Your full name", with: "Mary")
+    click_button "Continue"
+
+    # Upload your ID
+    attach_file("request-form-requester-photo-field", "spec/fixtures/files/file.jpg")
+    attach_file("request-form-requester-proof-of-address-field", "spec/fixtures/files/file.jpg")
+    click_button "Continue"
+
+    # Confirm upload
+    choose "Yes, add these uploads"
+    click_button "Continue"
+
+    # Letter of Consent
+    attach_file("request-form-letter-of-consent-field", "spec/fixtures/files/file.jpg")
+    click_button "Continue"
+
+    # Confirm upload
+    choose "Yes, add this upload"
+    click_button "Continue"
+
+    # Upload their ID
+    attach_file("request-form-subject-photo-field", "spec/fixtures/files/file.jpg")
+    attach_file("request-form-subject-proof-of-address-field", "spec/fixtures/files/file.jpg")
+    click_button "Continue"
+
+    # Confirm upload
+    choose "Yes, add these uploads"
+    click_button "Continue"
+
+    # Information from where
+    check "Somewhere else in the Ministry of Justice"
+    fill_in("Where in the Ministry of Justice do you think this information is held?", with: "location where")
+    click_button "Continue"
+
+    # Other information
+    fill_in("request-form-moj-other-text-field", with: "information required")
+    click_button "Continue"
+
+    # Date range
+    click_button "Continue"
+
+    # Requester address
+    fill_in("Your address", with: "1 High Street")
+    click_button "Continue"
+
+    # Your email
+    click_button "Continue"
+
+    # Upcoming case?
+    choose "No"
+    click_button "Continue"
+
+    # Check answers
+    expect(page).to have_text("Check your answers")
+    click_button "Accept and send request"
+
+    # Form sent
+    expect(page).to have_text("Request sent")
+  end
+end
---
+++ spec/features/request_by_solicitor_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+
+RSpec.feature "Request by solicitor", type: :feature do
+  scenario "User makes end to end request" do
+    visit "/"
+
+    # Start page
+    click_link "Start now"
+
+    # Subject
+    choose "Someone else's"
+    click_button "Continue"
+
+    # Your details
+    fill_in("Full name", with: "John")
+    click_button "Continue"
+
+    # Date of birth
+    fill_in("Day", with: "1")
+    fill_in("Month", with: "1")
+    fill_in("Year", with: "2000")
+    click_button "Continue"
+
+    # Relationship
+    choose "Legal representative"
+    click_button "Continue"
+
+    # Your details
+    fill_in("Name of your organisation", with: "Solicitor Name")
+    click_button "Continue"
+
+    # Letter of Consent
+    attach_file("request-form-letter-of-consent-field", "spec/fixtures/files/file.jpg")
+    click_button "Continue"
+
+    # Confirm upload
+    choose "Yes, add this upload"
+    click_button "Continue"
+
+    # Information from where
+    check "Probation Service"
+    click_button "Continue"
+
+    # Probation office
+    fill_in("request-form-probation-office-field", with: "Leicester")
+    click_button "Continue"
+
+    # Types of information
+    check "Something else"
+    fill_in("Can you provide more detail?", with: "more info")
+    click_button "Continue"
+
+    # Date range
+    click_button "Continue"
+
+    # Requester address
+    fill_in("Your address", with: "1 High Street")
+    click_button "Continue"
+
+    # Your email
+    click_button "Continue"
+
+    # Upcoming case?
+    choose "Yes"
+    fill_in("Can you provide more details?", with: "Info about hearing")
+    click_button "Continue"
+
+    # Check answers
+    expect(page).to have_text("Check your answers")
+    click_button "Accept and send request"
+
+    # Form sent
+    expect(page).to have_text("Request sent")
+  end
+end
