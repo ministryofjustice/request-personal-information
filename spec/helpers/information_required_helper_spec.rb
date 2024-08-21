9d893042ad8aeab16cdad85541fc2314f7136a14
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-08-21 07:52:41 +0100

    correct syntax

--- spec/helpers/information_required_helper_spec.rb
+++ spec/helpers/information_required_helper_spec.rb
@@ -1,11 +1,10 @@
-
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe InformationRequiredHelper, type: :helper do
-  describe '#format_information' do
-    let(:information) {"Prison Service<br>Probation Service" }
+  describe "#format_information" do
+    let(:information) { "Prison Service<br>Probation Service" }
 
-    it 'returns an array of values when given a string' do
+    it "returns an array of values when given a string" do
       result = helper.format_information(information)
       expect(result).to eq(["Prison Service", "Probation Service"])
     end
