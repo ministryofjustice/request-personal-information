f3ef5802ebec21276d113cc9f8a91c4d73e3be6d
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-10-18 01:28:35 +0100

    Amend form

--- app/models/request_form/subject_id.rb
+++ app/models/request_form/subject_id.rb
@@ -2,14 +2,10 @@ module RequestForm
   class SubjectId < Base
     attribute :subject_photo
     attribute :subject_photo_id
-    attribute :subject_proof_of_address
-    attribute :subject_proof_of_address_id
     attr_accessor :default
 
     validates :subject_photo, presence: true, unless: -> { Attachment.exists?(subject_photo_id) }
     validates :subject_photo, file_size: { max: 7.megabytes }
-    validates :subject_proof_of_address, presence: true, unless: -> { Attachment.exists?(subject_proof_of_address_id) }
-    validates :subject_proof_of_address, file_size: { max: 7.megabytes }
 
     def required?
       !request.by_solicitor?
@@ -18,14 +14,12 @@ module RequestForm
     def saveable_attributes
       attrs = attributes.clone
       attrs.delete("subject_photo_id") if subject_photo_id.nil? || subject_photo.present?
-      attrs.delete("subject_proof_of_address_id") if subject_proof_of_address_id.nil? || subject_proof_of_address.present?
       attrs.delete("subject_photo") if subject_photo.nil?
-      attrs.delete("subject_proof_of_address") if subject_proof_of_address.nil?
       attrs
     end
 
     def updateable_attributes
-      attributes.except("subject_photo", "subject_proof_of_address")
+      attributes.except("subject_photo")
     end
   end
 end
