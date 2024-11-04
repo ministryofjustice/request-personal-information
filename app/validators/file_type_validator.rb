8b67c8e7fc429139caea83fe5f60081e1cb22393
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-11-04 00:22:58 +0000

    fix rubocop errors

--- app/validators/file_type_validator.rb
+++ app/validators/file_type_validator.rb
@@ -1,12 +1,11 @@
- class FileTypeValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      return unless value
-      debugger
+class FileTypeValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return unless value
 
-      allowed_types = options[:allowed] ||  %w[image/jpg image/jpeg image/png application/pdf application/doc application/docx]
+    allowed_types = options[:allowed] || %w[image/jpg image/jpeg image/png application/pdf application/doc application/docx]
 
-      if !allowed_types.include?(value.content_type)
-        record.errors.add(attribute, :invalid_file_type)
-      end
+    unless allowed_types.include?(value.content_type)
+      record.errors.add(attribute, :invalid_file_type)
     end
   end
+end
