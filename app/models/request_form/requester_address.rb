3d14552aa1dc115c408050d89f64c1e7175ba242
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-11-10 21:18:49 +0000

    separate photo and address page

---
+++ app/models/request_form/requester_address.rb
@@ -0,0 +1,25 @@
+module RequestForm
+  class RequesterId < Base
+    attribute :requester_proof_of_address
+    attribute :requester_proof_of_address_id
+    attr_accessor :default
+
+    validates :requester_proof_of_address, presence: true, unless: -> { Attachment.exists?(requester_proof_of_address_id) }
+    validates :requester_proof_of_address, file_size: { max: 7.megabytes }
+
+    def required?
+      request.by_family_or_friend?
+    end
+
+    def saveable_attributes
+      attrs = attributes.clone
+      attrs.delete("requester_proof_of_address_id") if requester_proof_of_address_id.nil? || requester_proof_of_address.present?
+      attrs.delete("requester_proof_of_address") if requester_proof_of_address.nil?
+      attrs
+    end
+
+    def updateable_attributes
+      attributes.except("requester_photo")
+    end
+  end
+end
--- app/models/request_form/requester_id.rb
+++ app/models/request_form/requester_id.rb
@@ -2,14 +2,11 @@ module RequestForm
   class RequesterId < Base
     attribute :requester_photo
     attribute :requester_photo_id
-    attribute :requester_proof_of_address
-    attribute :requester_proof_of_address_id
+
     attr_accessor :default
 
     validates :requester_photo, presence: true, unless: -> { Attachment.exists?(requester_photo_id) }
     validates :requester_photo, file_size: { max: 7.megabytes }
-    validates :requester_proof_of_address, presence: true, unless: -> { Attachment.exists?(requester_proof_of_address_id) }
-    validates :requester_proof_of_address, file_size: { max: 7.megabytes }
 
     def required?
       request.by_family_or_friend?
@@ -18,9 +15,7 @@ module RequestForm
     def saveable_attributes
       attrs = attributes.clone
       attrs.delete("requester_photo_id") if requester_photo_id.nil? || requester_photo.present?
-      attrs.delete("requester_proof_of_address_id") if requester_proof_of_address_id.nil? || requester_proof_of_address.present?
       attrs.delete("requester_photo") if requester_photo.nil?
-      attrs.delete("requester_proof_of_address") if requester_proof_of_address.nil?
       attrs
     end
 
