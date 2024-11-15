1e751e6ce8957ee453590b0e38b87dde32998a9b
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-11-15 10:50:45 +0000

    fix failing test

--- spec/models/request_form/requester_address_spec.rb
+++ spec/models/request_form/requester_address_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe RequestForm::RequesterAddress, type: :model do
-  it_behaves_like "question when requester is not a solicitor"
+  it_behaves_like "question for friend or family of subject"
   it_behaves_like "file upload", :requester_proof_of_address
 
   describe "validation" do
