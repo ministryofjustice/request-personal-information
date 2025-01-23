af7bf195d2a99f785893b54a7b80ff2e37778d67
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2025-01-23 22:07:08 +0000

    Write the tests for feedback page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe "Pages", type: :request do
       expect(response.body).to include("Request personal information from the Ministry of Justice")
     end
   end
+  describe "#feedbackpage" do
+    it "shows the feedback page" do
+      get feedback_path
+      expect(response).to be_successful
+      expect(response.body).to include("Give feedback on this service")
+      expect(response.body).to include("Overall, how did you feel about the service you received today?")
+      expect(response.body).to include("How could we improve this service?")
+    end
+  end
 end
