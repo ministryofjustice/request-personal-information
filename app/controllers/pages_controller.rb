7ccddf1e2410e164cb2e91ae63c67a8ba246a9de
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2025-01-28 15:54:06 +0000

    correct syntax

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
   def homepage; end
 
   def feedback
-      @feedback = Feedback.new  # Create a dummy feedback object
+    @feedback = Feedback.new # Create a dummy feedback object
   end
 
 private
+
   def feedback_params
-    params.require(:feedback).permit(:service_satisfaction_level, :comments) #whitelist submitted parameters
+    params.require(:feedback).permit(:service_satisfaction_level, :comments) # whitelist submitted parameters
   end
 end
-
