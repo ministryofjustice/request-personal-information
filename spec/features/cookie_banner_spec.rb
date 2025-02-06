6d8c9bde7a26b4133789d4b7997a6634872d01e7
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2025-02-06 23:00:11 +0000

    add tests for new components

--- spec/features/cookie_banner_spec.rb
+++ spec/features/cookie_banner_spec.rb
@@ -2,22 +2,24 @@ require "rails_helper"
 
 RSpec.feature "CookieBanner", :js, type: :feature do
   before do
-    visit root_path
+    visit "/"
   end
 
-  scenario "User sees the cookie banner" do
+  scenario "User sees the cookie banner on the homepage" do
     expect(page).to have_content("We use some essential cookies to make this service work.")
   end
 
   scenario "User accepts analytics cookies" do
-    expect(page).to have_content("We use some essential cookies to make this service work.")
+    expect(page).to have_content("We use some essential cookies to make this service work.", wait: 10 )
     click_button "Accept analytics cookies"
-    expect(page).not_to have_content("We use some essential cookies to make this service work.")
+    expect(page).not_to have_content("We use some essential cookies to make this service work.", wait: 10)
+    expect(page).to have_content("You’ve accepted analytics cookies.", wait: 10 )
   end
 
   scenario "User rejects analytics cookies" do
-    expect(page).to have_content("We use some essential cookies to make this service work.")
+    expect(page).to have_content("We use some essential cookies to make this service work.", wait: 10 )
     click_button "Reject analytics cookies"
-    expect(page).not_to have_content("We use some essential cookies to make this service work.")
+    expect(page).not_to have_content("We use some essential cookies to make this service work.", wait: 10 )
+    expect(page).to have_content("You’ve rejected analytics cookies. You can change your cookie settings at any time.", wait: 10 )
   end
 end
---
+++ spec/features/cookie_settings_spec.rb
@@ -0,0 +1,24 @@
+# spec/features/cookie_settings_spec.rb
+require 'rails_helper'
+
+RSpec.feature "CookieSettings", type: :feature do
+  scenario "User accepts analytics cookies" do
+    visit cookies_path
+
+    choose 'Yes'
+    click_button 'Save cookie settings'
+
+    expect(page).to have_content('You’ve accepted analytics cookies.')
+    expect(page.driver.browser.rack_mock_session.cookie_jar['rpi_cookies_consent']).to eq('accept')
+  end
+
+  scenario "User rejects analytics cookies" do
+    visit cookies_path
+
+    choose 'No'
+    click_button 'Save cookie settings'
+
+    expect(page).to have_content('You’ve rejected analytics cookies.')
+    expect(page.driver.browser.rack_mock_session.cookie_jar['rpi_cookies_consent']).to eq('reject')
+  end
+end
