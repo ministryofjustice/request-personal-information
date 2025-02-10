1bc216d39abece20934ba0b90dd962f914eca11b
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-02-10 17:08:11 +0000

    Hide cookie banner

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 import { initAll } from "govuk-frontend";
 
 initAll();
+
+// Hide cookie banner
+var cookieBanners = document.getElementsByClassName("govuk-cookie-banner");
+if (cookieBannerElements.length) {
+  var cookieBanner = cookieBanners[0];
+  var hideButtons = cookieBanner.getElementsByClassName('cookie-hide-button');
+
+  if (hideButtons.length) {
+    var hideButton = hideButtons[0];
+    hideButton.addEventListener("click", function(e) {
+      e.preventDefault();
+      cookieBanner.style.display = 'none';
+    });
+  }
+}
