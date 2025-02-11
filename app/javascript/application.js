98cb49c905532c6a93a24c3793d900cee8d085d3
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-02-11 10:51:07 +0000

    Improve JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,13 +3,11 @@ import { initAll } from "govuk-frontend";
 initAll();
 
 // Hide cookie banner
-var cookieBanners = document.getElementsByClassName("govuk-cookie-banner");
-if (cookieBannerElements.length) {
-  var cookieBanner = cookieBanners[0];
-  var hideButtons = cookieBanner.getElementsByClassName('cookie-hide-button');
+var cookieBanner = document.querySelector(".govuk-cookie-banner");
+if (cookieBanner) {
+  var hideButton = cookieBanner.querySelector('.cookie-hide-button');
 
-  if (hideButtons.length) {
-    var hideButton = hideButtons[0];
+  if (hideButton) {
     hideButton.addEventListener("click", function(e) {
       e.preventDefault();
       cookieBanner.style.display = 'none';
