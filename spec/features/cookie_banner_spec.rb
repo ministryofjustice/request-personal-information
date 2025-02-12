fb84efeb1d41f3abccfcf798c6fd89f5d5ec6897
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-02-12 14:53:12 +0000

    fix cookie feature tests

--- spec/features/cookie_banner_spec.rb
+++ spec/features/cookie_banner_spec.rb
@@ -23,23 +23,3 @@ RSpec.feature "Cookie Banner", type: :feature do
     expect(page).to have_content("You've rejected analytics cookies. You can change your cookie settings at any time.")
   end
 end
-
-RSpec.feature "Cookies page", type: :feature do
-  before do
-    visit "/cookies"
-  end
-
-  scenario "User does not want to accept analytics cookies" do
-    choose "No"
-    click_button "Save cookie settings"
-    expect(page).not_to have_content("We use some essential cookies to make this service work.")
-    expect(page).to have_content("You've rejected analytics cookies. You can change your cookie settings at any time.")
-  end
-
-  scenario "User wants to accept analytics cookies" do
-    choose "Yes"
-    click_button "Save cookie settings"
-    expect(page).not_to have_content("We use some essential cookies to make this service work.")
-    expect(page).to have_content("You've accepted analytics cookies.")
-  end
-end
