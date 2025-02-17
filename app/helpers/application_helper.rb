73638743f84e2746dd64a7fb37c5177fb1156433
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-02-17 16:37:52 +0000

    Strip whitespace and add tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def title(page_title)
-    content_for :page_title, [page_title.presence, service_name].compact.join(" - ")
+    content_for :page_title, [page_title.presence&.strip, service_name].compact.join(" - ")
   end
 
   def service_name
