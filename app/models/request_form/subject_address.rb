38452d4ac15205420067619b31b871987b81cf54
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-04-14 11:11:03 +0100

    CDPT-2497 Antivirus (#223)
    Co-authored-by: Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
    Co-authored-by: Hettie Street <43522239+HettieS@users.noreply.github.com>

--- app/models/request_form/subject_address.rb
+++ app/models/request_form/subject_address.rb
@@ -5,7 +5,7 @@ module RequestForm
     attr_accessor :default
 
     validates :subject_proof_of_address, presence: true, unless: -> { Attachment.exists?(subject_proof_of_address_id) }
-    validates :subject_proof_of_address, file_size: { max: 7.megabytes }, file_type: true
+    validates :subject_proof_of_address, file_size: { max: 7.megabytes }, file_type: true, anti_virus: true
 
     def required?
       !request.by_solicitor?
