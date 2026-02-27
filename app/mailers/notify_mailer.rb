d32fe50764aa77f765e25c74ca7937911cfcb80c
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-02-27 09:24:38 +0000

    Modify email template subject line

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -3,6 +3,7 @@ class NotifyMailer < ApplicationMailer
   def new_request(request)
     @information_request = request
     @summary = request.summary
-    view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: request.contact_email, subject: "Request Personal Information")
+    # view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: request.contact_email, subject: "Request Personal Information")
+    view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: request.contact_email, subject: @information_request.name, @information_request.form_date_of_birth)
   end
 end
