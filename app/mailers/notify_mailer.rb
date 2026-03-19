5363547b07db9965d77c2578a93a40c6a0df7819
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-03-19 10:48:12 +0000

    testing recipient dept fork

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -4,12 +4,13 @@ class NotifyMailer < ApplicationMailer
     @information_request = request
     @summary = request.summary
 
-    if request.probation_service == true || request.prison_service
-      @recipient = OFFENDER_SAR_EMAIL
-    else
-      @recipient = SAR_EMAIL
-    
+    forwarding_department = if request.probation_service? || request.prison_service?
+                              "nick.preddy @justice.gov.uk"
+                            else
+                              "nick.preddy @digital.Justice.gov.uk"
+                            end
+
     # view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: request.contact_email, subject: "Submission from Request personal information #{@information_request.full_name} DoB #{@information_request.date_of_birth.strftime('%d/%m/%Y')}")
-    view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: @recipient, subject: "Submission from Request personal information #{@information_request.full_name} DoB #{@information_request.date_of_birth.strftime('%d/%m/%Y')}")
+    view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: forwarding_department, subject: "Submission from Request personal information #{@information_request.full_name} DoB #{@information_request.date_of_birth.strftime('%d/%m/%Y')}")
   end
 end
