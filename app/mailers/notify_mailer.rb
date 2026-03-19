82931b5a216b18591b9a160c71d95c43dcea306e
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-03-19 09:22:13 +0000

    set recipient department

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -3,6 +3,13 @@ class NotifyMailer < ApplicationMailer
   def new_request(request)
     @information_request = request
     @summary = request.summary
-    view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: request.contact_email, subject: "Submission from Request personal information #{@information_request.full_name} DoB #{@information_request.date_of_birth.strftime('%d/%m/%Y')}")
+
+    if request.probation_service == true || request.prison_service
+      @recipient = OFFENDER_SAR_EMAIL
+    else
+      @recipient = SAR_EMAIL
+    
+    # view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: request.contact_email, subject: "Submission from Request personal information #{@information_request.full_name} DoB #{@information_request.date_of_birth.strftime('%d/%m/%Y')}")
+    view_mail("ed38a7b5-d9d1-44a5-bdc3-0a3319fa54c6", to: @recipient, subject: "Submission from Request personal information #{@information_request.full_name} DoB #{@information_request.date_of_birth.strftime('%d/%m/%Y')}")
   end
 end
